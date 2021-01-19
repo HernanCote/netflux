6568bd43cdf8709ed53c0b627a3e5bec468da001
Hernan Cote <hernan.cote@zemoga.com>
2021-01-19 18:57:00 -0500

    Initial files

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './app';
+import reportWebVitals from './reportWebVitals';
+
+render(<App />, document.getElementById('root'));
+
+reportWebVitals();
