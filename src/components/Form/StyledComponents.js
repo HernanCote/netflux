9bd2620426b593672d3b22c6e57fbe956f799702
Hernan Cote <hernan.cote@zemoga.com>
2021-04-28 16:15:18 -0500

    Completed sign in component

--- src/components/Form/StyledComponents.js
+++ src/components/Form/StyledComponents.js
@@ -1,19 +1,57 @@
 import styled from 'styled-components/macro';
-import { Link as ReachRouterLink } from 'react-router-dom';
+import { Link as ReactRouterLink } from 'react-router-dom';
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 41.25rem;
+  background-color: rgba(0, 0, 0, 0.75);
+  border-radius: 0.25rem;
+  box-sizing: border-box;
+  width: 100%;
+  margin: auto;
+  max-width: 28.25rem;
+  padding: 3.75rem 4.25rem 2.5rem;
+  margin-bottom: 6.25rem;
+`;
 
-const Error = styled.div``;
+const Error = styled.div`
+  background: #e87c03;
+  border-radius: 0.25rem;
+  font-size: 0.75rem;
+  margin: 0 0 1rem;
+  color: #fff;
+  padding: 1rem 1.25rem;
+`;
 
-const Base = styled.form``;
+const Base = styled.form`
+  display: flex;
+  flex-direction: column;
+  max-width: 28.25rem;
+  width: 100%;
+`;
 
-const Title = styled.h1``;
+const Title = styled.h1`
+  color: #fff;
+  font-size: 2rem;
+  font-weight: bold;
+  margin-bottom: 1.75rem;
+`;
 
-const Text = styled.p``;
+const Text = styled.p`
+  color: #737373;
+  font-size: 1rem;
+  font-weight: 500;
+`;
 
-const TextSmall = styled.p``;
+const TextSmall = styled.p`
+  margin-top: 0.5rem;
+  font-size: 0.75rem;
+  line-height: normal;
+  color: #8c8c8c;
+`;
 
-const Link = styled(ReachRouterLink)`
+const Link = styled(ReactRouterLink)`
   color: white;
   text-decoration: none;
 
@@ -22,9 +60,37 @@ const Link = styled(ReachRouterLink)`
   }
 `;
 
-const Input = styled.input``;
+const Input = styled.input`
+  background: #333;
+  border-radius: 0.25rem;
+  border: 0;
+  color: #fff;
+  height: 3.25rem;
+  line-height: 3.25rem;
+  padding: 0.25rem 1.25rem;
+  margin-bottom: 1.25rem;
 
-const Submit = styled.button``;
+  &:last-of-type {
+    margin-bottom: 2rem;
+  }
+`;
+
+const Submit = styled.button`
+  background: #e50914;
+  border-radius: 0.25rem;
+  font-size: 1rem;
+  font-weight: bold;
+  margin: 1.5rem 0 0.75rem;
+  padding: 1rem;
+  border: 0;
+  color: #fff;
+  cursor: pointer;
+
+  &:disabled {
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
+`;
 
 export {
   Container,
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -7,9 +7,15 @@ import {
 } from '../../containers';
 
 const SignIn = () => {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+
+  const submitHandler = e => {
+    e.preventDefault();
+  };
+
+  const isInvalid = password === '' || email === '';
 
   return (
     <>
@@ -19,6 +25,31 @@ const SignIn = () => {
             Sign In
           </Form.Title>
           {error && <Form.Error>{error}</Form.Error>}
+          <Form.Base onSubmit={submitHandler}>
+            <Form.Input
+              placeholder="Email Address"
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+            />
+            <Form.Input
+              type="password"
+              autoComplete="off"
+              placeholder="Password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit
+              disabled={isInvalid}
+              type="submit"
+            >Sign In
+            </Form.Submit>
+          </Form.Base>
+          <Form.Text>
+            New to Netflux? <Form.Link to="/signup">Sign up now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCaptcha to ensure you are not a bot. Learn more.
+          </Form.TextSmall>
         </Form>
       </Header>
       <Footer />
